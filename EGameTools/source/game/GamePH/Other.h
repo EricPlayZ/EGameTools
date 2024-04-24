e0a15ee70d31fc482e08bbfb54c81c933326bd96
EricPlayZ <eric.jarnea12345361@gmail.com>
2024-04-24 04:04:15 +0300

    Update Other.h

--- EGameTools/source/game/GamePH/Other.h
+++ EGameTools/source/game/GamePH/Other.h
@@ -3,9 +3,9 @@
 #include <basetsd.h>
 
 namespace GamePH {
-	extern const std::tuple<uint16_t, uint16_t, uint16_t, uint16_t> GetCurrentGameVersion();
+	extern const DWORD GetCurrentGameVersion();
+	extern const std::string GameVerToStr(DWORD version);
 	extern const std::string GetCurrentGameVersionStr();
-	extern const std::string GameVerToStr(uint16_t version);
 	extern void ShowTPPModel(bool showTPPModel);
 	extern bool ReloadJumps();
 }
