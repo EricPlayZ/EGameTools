df811ca13416b5a64dbfe76adbf1b8e11181ea15
EricPlayZ <eric.jarnea12345361@gmail.com>
2024-04-24 04:47:30 +0300

    fix tpp model

--- EGameTools/source/game/GamePH/gen_TPPModel.h
+++ EGameTools/source/game/GamePH/gen_TPPModel.h
@@ -5,8 +5,8 @@ namespace GamePH {
 	class gen_TPPModel {
 	public:
 		union {
-			buffer<0x2D59, bool> enableTPPModel1;
-			buffer<0x2D5A, bool> enableTPPModel2;
+			buffer<0x2DB9, bool> enableTPPModel1;
+			buffer<0x2DBA, bool> enableTPPModel2;
 		};
 
 		static gen_TPPModel* Get();
